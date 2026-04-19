9ca86f017f34c0afec8f14b2d2a93900f4c8dd5a
Haze <marohimself@gmail.com>
2026-04-19 15:51:11 +0300

    fix logout redirect

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,10 +1,11 @@
 import { createClient } from '@/lib/supabase/server'
 import { NextResponse } from 'next/server'
 
-export async function POST() {
+export async function POST(request: Request) {
   const supabase = await createClient()
 
   await supabase.auth.signOut()
 
-  return NextResponse.redirect(new URL('/login', 'http://localhost:3000'))
+  const url = new URL('/login', request.url)
+  return NextResponse.redirect(url)
 }
